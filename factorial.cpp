85dc38074346fe3b47f3b671f5a1075d58705cdd
code-ash-IIT <b20087@students.iitmandi.ac.in>
2022-08-29 16:06:32 +0530

    11. fact files changed again

--- factorial.cpp
+++ factorial.cpp
@@ -4,8 +4,7 @@ int factorial(int n){
     if(n<0){
         return -1;
     }
-    if(n!=1){
+    if(n==0 || n==1)
+        return 1;
 	return(n * factorial(n-1));
-    }
-    else return 1;
 }
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(){
     print_hello();
     int n;cin>>n;
     int ans = factorial(n);
+    cout<<"By group-5\n";
     cout<<(ans == -1?"Invalid input, re-enter by again starting program":"Factorial of "+to_string(n)+" is "+to_string(ans));
     return 0;
 }
