3a16e27a4a8d73f28e9dee047c2be55377b88053
vkodesia21 <vanshkodesia@gmail.com>
2022-09-11 20:08:34 +0530

    print gcd

--- main.cpp
+++ main.cpp
@@ -10,5 +10,10 @@ int main(){
     if(ans==-1)printf("Invalid input, re-enter by again starting program. Status : %d",ans);
     else printf("Factorial of %d is %d",n,ans);
     // cout<<(ans == -1?"Invalid input, re-enter by again starting program":"Factorial of "+to_string(n)+" is "+to_string(ans));
+    int x,y;
+    x=56;
+    y=98;
+    int g = gcd(x,y);
+    cout<<"GCD of "<<x<<" and "<<y<<" is "<<gcd(x,y);
     return 0;
 }
